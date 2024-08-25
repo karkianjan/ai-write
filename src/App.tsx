3fdfb7c58586969f1cc074e85ced609e3029ae56
karkianjan <carkey.anjan2018@gmail.com>
2024-08-25 13:48:00 +0545

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+     <div className='text-red-600'>Hello! Ai write</div>
+     <button>Click Me</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import './assets/styles/global.css'
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
